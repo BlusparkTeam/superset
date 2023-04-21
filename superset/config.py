32814a36af7c9d138b285a2836fc418f2bf8dcef
mael <mael.kermann@bluspark.io>
2023-04-21 17:35:58 +0200

    Français par défaut + ajout de la possibilité de changer de langue

--- superset/config.py
+++ superset/config.py
@@ -359,18 +359,18 @@ BABEL_DEFAULT_FOLDER = "superset/translations"
 LANGUAGES = {
     "en": {"flag": "us", "name": "English"},
     "es": {"flag": "es", "name": "Spanish"},
-    "it": {"flag": "it", "name": "Italian"},
+#     "it": {"flag": "it", "name": "Italian"},
     "fr": {"flag": "fr", "name": "French"},
-    "zh": {"flag": "cn", "name": "Chinese"},
-    "ja": {"flag": "jp", "name": "Japanese"},
-    "de": {"flag": "de", "name": "German"},
-    "pt": {"flag": "pt", "name": "Portuguese"},
+#     "zh": {"flag": "cn", "name": "Chinese"},
+#     "ja": {"flag": "jp", "name": "Japanese"},
+#     "de": {"flag": "de", "name": "German"},
+#     "pt": {"flag": "pt", "name": "Portuguese"},
     "pt_BR": {"flag": "br", "name": "Brazilian Portuguese"},
-    "ru": {"flag": "ru", "name": "Russian"},
-    "ko": {"flag": "kr", "name": "Korean"},
-    "sk": {"flag": "sk", "name": "Slovak"},
-    "sl": {"flag": "si", "name": "Slovenian"},
-    "nl": {"flag": "nl", "name": "Dutch"},
+#     "ru": {"flag": "ru", "name": "Russian"},
+#     "ko": {"flag": "kr", "name": "Korean"},
+#     "sk": {"flag": "sk", "name": "Slovak"},
+#     "sl": {"flag": "si", "name": "Slovenian"},
+#     "nl": {"flag": "nl", "name": "Dutch"},
 }
 # Turning off i18n by default as translation in most languages are
 # incomplete and not well maintained.
